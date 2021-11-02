8fc8b92818ee55a2134d6cec0693a9bff3fdb823
pj <pete.whitby@octavian.co.uk>
2021-11-02 20:11:15 +0000

    Add relay example

---
+++ Relays/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+
+namespace Relays
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Relay!, ctrl+C to quit");
+
+            var r1 = 10;
+            var r2 = 12;
+            var r3 = 16;
+            var r4 = 18;
+
+            using var controller = new GpioController(PinNumberingScheme.Board);
+
+            // High is off
+            controller.OpenPin(r1, PinMode.Output, PinValue.High);
+            controller.OpenPin(r2, PinMode.Output, PinValue.High);
+            controller.OpenPin(r3, PinMode.Output, PinValue.High);
+            controller.OpenPin(r4, PinMode.Output, PinValue.High);
+
+            while (true)
+            {
+                Toggle(controller, r1);
+                Toggle(controller, r2);
+                Toggle(controller, r3);
+                Toggle(controller, r4);
+            }
+        }
+
+
+        private static void Toggle(GpioController controller, int r)
+        {
+            Console.WriteLine($"{r} Low (ON)");
+            controller.Write(r, PinValue.Low);
+            Thread.Sleep(2000);
+
+            Console.WriteLine($"{r} High (OFF)");
+            controller.Write(r, PinValue.High);
+            Thread.Sleep(2000);
+        }
+    }
+}
